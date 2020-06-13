73673c6abe8aac8232605629fcd7a02adf1652b1
AnandhiD <Anandhi.Dharmarajan@ksdc.co.za>
2020-06-13 08:04:15 +0200

    Article Detail activity changes

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -242,6 +242,7 @@ public class ArticleDetailFragment extends Fragment implements
                                 Palette p = Palette.generate(bitmap, 12);
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
+                                Log.d("anandhi","Anandhi"+imageContainer.getBitmap().toString());
                                 mRootView.findViewById(R.id.meta_bar)
                                         .setBackgroundColor(mMutedColor);
                                 updateStatusBar();
